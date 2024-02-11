2f8c109914710d28e853971ca052d71c11f52821
Victor Martins <vitor_jhoul@hotmail.com>
2024-02-11 15:05:03 -0300

    ajuste de css com tailwind

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,13 +1,21 @@
+import { PropsWithChildren } from "react";
 import star from '../../assets/Star.svg'
 import starfILL from '../../assets/Star_fill.svg'
+import { ICoffelist } from './../main/index';
 
-const Card = () => {
+interface CardsPRops  {
+  coffe: ICoffelist
+}
+
+
+
+const Card = ({coffe} : CardsPRops) => {
   return ( 
     <div>
       <span className="absolute bg-[#F6C768] font-sans font-bold mt-2 ml-2 px-3 text-xs rounded-full">Popular</span>
       <img className="rounded-lg" src="https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/house-coffee.jpg" alt="" />
       <div className="flex justify-between items-center mt-3  font-sans font-bold ">
-      <span className="text-slate-100">House Coffee</span> 
+      <span className="text-slate-100">{coffe.name}</span> 
       <span className="bg-[#BEE3CC] p-1 rounded-md text-[#111315] text-sm">$5.20</span>
       </div>
       <div>
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,23 +1,60 @@
-import imgHeader from '../../assets/vector.svg'
+import { useEffect, useState } from "react";
+import dbJson from '../../utilis/db.json'
 import Card from "../card";
-const Main = () => {
-  return ( 
-    <div className="bg-[#1B1D1F] flex flex-col items-center justify-center w-3/4 m-auto h-screen fixed lg:top-96 md:top-32 inset-0 top-14 rounded-lg">
 
-      <div className="flex flex-col items-center justify-center m-8 overflow-hidden pb-5">       
-        {/* <img className="absolute top-[15%] -right-28 md:right-9 md:top-40" src={imgHeader} alt="" /> */}
-        <h1 className="text-[32px] text-[#FEF7EE] font-sans ">Our Collection </h1>
-        <p className="text-[#6F757C] text-[16px] w-8/12 lg:w-7/12 font-sans">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>        
-        <div className="flex gap-3 text-sm mt-5">
-          <button className="bg-[#6F757C] p-2  rounded-md" >All Products</button>
-          <button className="bg-[#6F757C] p-2 rounded-md" >Available Now</button>
+export interface ICoffelist {
+  id: number,
+  name: string,
+  image: string,
+  price: string,
+  rating: number,
+  votes: number,
+  popular: boolean,
+  available: boolean
+}
+
+
+const Main = () => {
+  const [coffeList, setCoffeList] = useState<ICoffelist[]>([]);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json')
+      .then(response => response.json())
+      .then((data: ICoffelist[]) => {
+        setCoffeList(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <>
+      <div className="bg-[#1B1D1F] flex flex-col absolute items-center justify-center w-3/4 top-16 right-16 md:top-28 md:right-28 lg:top-48 lg:right-48 rounded-lg">
+
+        <div className=" w-full md:w-3/4 m-auto">
+          <div className="flex flex-col items-center justify-center m-8">
+            {/* <img className="absolute top-[15%] -right-28 md:right-9 md:top-40" src={imgHeader} alt="" /> */}
+            <h1 className="text-[32px] text-[#FEF7EE] font-sans ">Our Collection </h1>
+            <p className="text-[#6F757C] text-[16px] w-full md:w-8/12 font-sans">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+            <div className="flex gap-3 text-sm mt-5">
+              <button className="bg-[#6F757C] p-2  rounded-md" >All Products</button>
+              <button className="bg-[#6F757C] p-2 rounded-md" >Available Now</button>
+            </div>
+          </div>
         </div>
+
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 mb-20">
+          {coffeList.map((coffe) => (
+            <Card key={coffe.id} coffe={coffe} />
+          ))}
+        </div>
+
       </div>
 
-      <Card />
-    </div>
-   );
+
+    </>
+  );
 }
- 
+
 export default Main;
-// absolute md:top-[5.5rem]  inset-x-2/4
